654423743831233a0e032a5d4fbde7a749bf4950
TH Huang <tzhsn.huang@gmail.com>
2023-03-01 16:20:50 -0600

    [app] add default middlewares

--- app/main.go
+++ app/main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 
 	authApi "github.com/thhuang/go-server/app/handlers/auth"
+)
+
+var (
+	limiterConfig = limiter.Config{Max: 100, Expiration: 30 * time.Second}
 )
 
 func main() {
 	app := fiber.New()
 
+	// Default middlewares
 	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(requestid.New())
+	app.Use(limiter.New(limiterConfig))
 
+	// Handlers
 	authApi.New(app.Group("/auth"))
 
 	log.Fatal(app.Listen(":3000"))
