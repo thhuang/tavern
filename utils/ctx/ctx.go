b4d02e7d3e8a679ad1e474d99152197a9be9c84c
TH Huang <tzhsn.huang@gmail.com>
2023-03-11 15:01:23 -0600

    [utils] include logger in the context (#10)

--- utils/ctx/ctx.go
+++ utils/ctx/ctx.go
@@ -4,19 +4,42 @@ import (
 	"context"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/thhuang/go-server/utils/clock"
 )
 
+type logKey string
+
 type CTX struct {
 	context.Context
-	// logrus.FieldLogger
+	logrus.FieldLogger
 	clock.Clock
 }
 
 func Backbround() CTX {
 	return CTX{
-		Context: context.Background(),
-		// FieldLogger: logrus.StandardLogger(),
-		Clock: clock.New(time.Now()),
+		Context:     context.Background(),
+		FieldLogger: logrus.StandardLogger(),
+		Clock:       clock.New(time.Now()),
 	}
+}
+
+func (context CTX) Now() time.Time {
+	return context.Clock.Now().UTC()
+}
+
+func WithValue(parent CTX, key string, val interface{}) CTX {
+	return CTX{
+		Context:     context.WithValue(parent, logKey(key), val),
+		FieldLogger: parent.FieldLogger.WithField(key, val),
+		Clock:       parent.Clock,
+	}
+}
+
+func WithValues(parent CTX, kvs map[string]interface{}) CTX {
+	c := parent
+	for k, v := range kvs {
+		c = WithValue(c, k, v)
+	}
+	return c
 }
