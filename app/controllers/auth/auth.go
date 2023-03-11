66578bcaa2a4fd35ec74d4037be175c0a4fcdba4
TH Huang <tzhsn.huang@gmail.com>
2023-03-10 18:43:21 -0600

    [controller] add register endpoint

--- app/controllers/auth/auth.go
+++ app/controllers/auth/auth.go
@@ -2,6 +2,9 @@ package auth
 
 import (
 	"github.com/gofiber/fiber/v2"
+	authEntity "github.com/thhuang/go-server/app/entities/auth"
+	apiUtils "github.com/thhuang/go-server/utils/api"
+	errorModel "github.com/thhuang/go-server/utils/error"
 )
 
 func New(
@@ -10,12 +13,27 @@ func New(
 	h := handler{}
 
 	r.Get("/ping", h.ping)
+	r.Post("/register", h.register)
 }
 
 type handler struct{}
 
 func (h *handler) ping(c *fiber.Ctx) error {
-	return c.JSON(map[string]interface{}{
+	return c.JSON(fiber.Map{
 		"message": "pong",
+	})
+}
+
+func (h *handler) register(c *fiber.Ctx) error {
+	p := authEntity.UserAuth{}
+	if err := apiUtils.ParseBody(c, &p); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(errorModel.ErrorResponse{
+			Code:    errorModel.ErrorCodeUnknown,
+			Message: err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusInternalServerError).JSON(errorModel.ErrorResponse{
+		Code: errorModel.ErrorCodeNotImplemented,
 	})
 }
